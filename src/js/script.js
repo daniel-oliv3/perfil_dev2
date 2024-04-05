4e3c0f1069dbfb73dbe9895d87239c58c4ffeb15
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2024-04-05 10:33:14 -0300

    HTML

---
+++ src/js/script.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+/*
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+//alt + l + o = abre uma pagina no navegador com o projeto (LiveServer)
+//win + . = emoje
+
+//https://devicon.dev/
+
+/*
+Gerador de placeholder
+
+https://smalldev.tools/placeholder-image-generator-online
+*/
