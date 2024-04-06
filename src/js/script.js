85a2a3f94cd6e4c782cad9690557a5dde324dc16
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2024-04-06 19:42:55 -0300

    CSS

--- src/js/script.js
+++ src/js/script.js
@@ -21,10 +21,13 @@
 //alt + l + o = abre uma pagina no navegador com o projeto (LiveServer)
 //win + . = emoje
 
-//https://devicon.dev/
+//-> https://devicon.dev/
 
 /*
-Gerador de placeholder
+Gerador de placeholder_image
 
-https://smalldev.tools/placeholder-image-generator-online
+-> https://smalldev.tools/placeholder-image-generator-online
+
+Lineaar_gradient
+-> https://developer.mozilla.org/en-US/docs/Web/CSS/gradient/linear-gradient
 */
