291794b746c62fa8128d710d79849a6bd2439bf4
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2024-04-07 09:16:06 -0300

    JavaScript

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,45 @@
+// Objetivo 1 - quando o usuário clicar no botão de mostrar mais deve abrir os projetos que estão escondidos no html
 
+// Passo 1 - pegar o botão mostrar mais no JS pra poder verificar quando o usuário clicar em cima dele
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
 
+botaoMostrarProjetos.addEventListener('click', () => {
+    // Passo 3 - adicionar a classe "ativo" nos projetos escondidos
+    mostrarMaisProjetos();
+
+    // Objetivo 2 - esconder o botão de mostrar mais
+    // Passo 1 - pegar o botão e esconder ele
+    esconderBotao();
+});
+
+function esconderBotao() {
+    botaoMostrarProjetos.classList.add("remover");
+}
+
+function mostrarMaisProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.add('ativo');
+    });
+}
 
 
 
 /*
+    Objetivo 1 - quando o usuário clicar no botão de mostrar mais deve abrir os projetos que estão escondidos no html
 
+        Passo 1 - pegar o botão mostrar mais no JS pra poder verificar quando o usuário clicar em cima dele
+
+        Passo 2 - identificar o clique no botão
+        
+        Passo 3 - adicionar a classe "ativo" nos projetos escondidos
+
+    Objetivo 2 - esconder o botão de mostrar mais
+        Passo 1 - pegar o botão e esconder ele
 */
 
 
-
-
-
-
-
-
-
-
-
-
+/* Atalhos */
 //alt + l + o = abre uma pagina no navegador com o projeto (LiveServer)
 //win + . = emoje
 
@@ -28,6 +50,9 @@ Gerador de placeholder_image
 
 -> https://smalldev.tools/placeholder-image-generator-online
 
-Lineaar_gradient
+Linear_gradient
 -> https://developer.mozilla.org/en-US/docs/Web/CSS/gradient/linear-gradient
+
+extensão: Responsive viewer
+-> https://chromewebstore.google.com/detail/responsive-viewer/inmopeiepgfljkpkidclfgbgbmfcennb?pli=1
 */
